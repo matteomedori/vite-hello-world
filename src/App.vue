62cc5624ba3939e2f9067172738aebe8a973bfa1
matteomedori <matteo.medori.97@gmail.com>
2023-12-06 15:06:52 +0100

    feat: bonus aggiunto jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script>
 import AppTitleComponent from "./components/AppTitleComponent.vue";
+import AppHeroComponent from "./components/AppHeroComponent.vue";
 
 export default {
   data() {},
   components: {
     AppTitleComponent,
+    AppHeroComponent,
   },
 };
 </script>
 
 <template>
   <AppTitleComponent />
+  <AppHeroComponent />
 </template>
 
 <style scoped></style>
---
+++ src/components/AppHeroComponent.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+      paragraphText:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, excepturi quos velit accusamus exercitationem culpa laborum sapiente aperiam veritatis, earum omnis expedita praesentium tempora quis libero dignissimos! Maiores, voluptatibus ex?",
+      jumboTitle: "Jumbotron",
+      imgUrl:
+        "https://www.raicultura.it/cropgd/900x520/dl/img/2020/04/08/1586351246504_abstract-2468874_1920.jpg",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="jumbo">
+    <p>
+      {{ paragraphText }}
+    </p>
+    <h2>{{ jumboTitle }}</h2>
+    <img :src="imgUrl" alt="img-jumbo" />
+  </div>
+</template>
+
+<style>
+.jumbo {
+  text-align: center;
+}
+
+h2 {
+  color: red;
+}
+
+img {
+  width: 400px;
+  height: 200px;
+}
+</style>
