c71b3335c3958bf94193d815dc4e5fd0e45e2b1f
matteomedori <matteo.medori.97@gmail.com>
2023-12-06 14:51:57 +0100

    feat: componente AppTitle

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppTitleComponent from "./components/AppTitleComponent.vue";
+
+export default {
+  data() {},
+  components: {
+    AppTitleComponent,
+  },
+};
+</script>
+
+<template>
+  <AppTitleComponent />
+</template>
+
+<style scoped></style>
---
+++ src/components/AppTitleComponent.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "La mia prima app con Vite!",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: green;
+  text-transform: uppercase;
+  text-align: center;
+}
+</style>
